4007ac90b05234135b409c67209b49647bb584f9
EruDev <1027926875@qq.com>
2018-07-21 10:10:14 +0800

    实现登录限制

--- apps/cms/views.py
+++ apps/cms/views.py
@@ -3,21 +3,25 @@ import os
 from flask import Blueprint, views, render_template, request, session, redirect, url_for
 from .forms import LoginForm
 from .models import CMSUser
+from .decorators import login_required
+
 
 bp = Blueprint('cms', __name__, url_prefix='/cms')
 
 @bp.route('/')
+@login_required
 def index():
     return 'cms index'
 
 
 class LoginView(views.MethodView):
 
-    def get(self, message=None):
-        return render_template('cms/cms_login.html', message=message)
+    def get(self, form=None):
+        form = LoginForm()
+        return render_template('cms/cms_login.html', form=form)
 
     def post(self):
-        form = LoginForm(request.form, csrf_enabled=False)
+        form = LoginForm(request.form)
 
         if form.validate_on_submit():
             email = form.email.data
@@ -32,12 +36,12 @@ class LoginView(views.MethodView):
                     session.permanent = True
                 return redirect(url_for('cms.index'))
             else:
-                return self.get(message='密码或邮箱错误')
+                return render_template('cms/cms_login.html', form=form, message='邮箱或密码错误')
         else:
             # 如果验证失败, 刷新登录页面
             message = form.errors.popitem()[1][0]
             print(message)
-            return self.get(message=message)
+            return render_template('cms/cms_login.html', form=form, message=message)
 
 
 bp.add_url_rule('/login/', view_func=LoginView.as_view('login'))
