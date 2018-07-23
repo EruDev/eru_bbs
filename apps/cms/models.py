4063fa453d95fc8d4d591df487ed9edffda75101
EruDev <1027926875@qq.com>
2018-07-23 17:59:31 +0800

    修改相关人员的权限

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -4,6 +4,43 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from exts import db
 
 
+class CMSPermission(object):
+    # 255的二进制方式来表示 1111 1111
+    ALL_PERMISSION = 0b11111111
+    # 1. 访问者权限
+    VISITOR =        0b00000001
+    # 2. 管理帖子权限
+    POSTER =         0b00000010
+    # 3. 管理评论的权限
+    COMMENTER =      0b00000100
+    # 4. 管理版块的权限
+    BOARDER =        0b00001000
+    # 5. 管理前台用户的权限
+    FRONTUSER =      0b00010000
+    # 6. 管理后台用户的权限
+    CMSUSER =        0b00100000
+    # 7. 管理后台管理员的权限
+    ADMINER =        0b01000000
+
+
+cms_role_user = db.Table(
+    'cms_role_user',
+    db.Column('cms_role.id', db.Integer, db.ForeignKey('cms_role.id'), primary_key=True),
+    db.Column('cms_user.id', db.Integer, db.ForeignKey('cms_user.id'), primary_key=True),
+)
+
+
+class CMSRole(db.Model):
+    __tablename__ = 'cms_role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), nullable=False)
+    desc = db.Column(db.String(128), nullable=True)
+    create_time = db.Column(db.DateTime, default=datetime.now)
+    permissions = db.Column(db.Integer, default=CMSPermission.VISITOR)
+
+    users = db.relationship('CMSUser', secondary=cms_role_user, backref='roles')
+
+
 class CMSUser(db.Model):
     __tablename__ = 'cms_user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
--- manage.py
+++ manage.py
@@ -5,6 +5,9 @@ from exts import db
 from apps.cms import models as cms_models
 
 CMSUser = cms_models.CMSUser
+CMSRole = cms_models.CMSRole
+CMSPermission = cms_models.CMSPermission
+
 
 app = create_app()
 manager = Manager(app)
@@ -20,6 +23,30 @@ def create_cms_user(username, password, email):
     db.session.add(user)
     db.session.commit()
     print('create cms user success!')
+
+
+@manager.command
+def create_role():
+    # 1. 访问者(可以修改个人信息)
+    visitor = CMSRole(name='访问者', desc='查看相关数据, 不能修改')
+    visitor.permissions = CMSPermission.VISITOR
+
+    # 2. 运营角色(修改个人信息, 管理帖子, 管理评论, 管理前台用户)
+    operator = CMSRole(name='运营', desc='管理帖子, 管理评论, 管理前台用户')
+    operator.permissions = CMSPermission.VISITOR|CMSPermission.POSTER\
+                          |CMSPermission.CMSUSER|CMSPermission.COMMENTER|CMSPermission.FRONTUSER
+
+    # 3. 管理员(拥有绝大部分权限)
+    admin = CMSRole(name='管理员', desc='拥有本系统所有权限')
+    admin.permissions = CMSPermission.VISITOR|CMSPermission.POSTER|CMSPermission.CMSUSER\
+                        |CMSPermission.COMMENTER|CMSPermission.FRONTUSER|CMSPermission.BOARDER
+
+    # 4. 开发者
+    developer = CMSRole(name='开发者', desc='开发人员专用角色')
+    developer.permissions = CMSPermission.ALL_PERMISSION
+
+    db.session.add_all([visitor, operator, admin, developer])
+    db.session.commit()
 
 
 if __name__ == '__main__':
